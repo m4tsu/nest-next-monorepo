b2ca47a3eecd0c16c80f506d3a98929f76228afb
m4tsu <matsurec0147@gmail.com>
2022-12-25 14:06:56 +0000

    init monorepo spi

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint/eslint-plugin", "import", "unused-imports"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: [".eslintrc.js"],
+  rules: {
+    "sort-imports": 0,
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        pathGroups: [
+          {
+            pattern: "@/**",
+            group: "parent",
+            position: "before",
+          },
+        ],
+        alphabetize: {
+          order: "asc",
+        },
+        "newlines-between": "always",
+      },
+    ],
+    // "@typescript-eslint/interface-name-prefix": "off",
+    // "@typescript-eslint/explicit-function-return-type": "off",
+    // "@typescript-eslint/explicit-module-boundary-types": "off",
+    // "@typescript-eslint/no-explicit-any": "off",
+  },
+};
---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:16.13.2-bullseye
+
+USER node
+WORKDIR /workspace
+
+# COPY package*.json ./
+# RUN npm ci
+
+# COPY . .
